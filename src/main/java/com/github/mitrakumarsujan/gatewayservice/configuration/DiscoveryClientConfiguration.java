b36f88d41649b604edf65d7fab0e5d66ba68f1b4
skmitra <mitrakumarsujan@gmail.com>
2020-12-12 16:04:58 +0530

    changes:: 1. integrated with survey app 2. added config client and eureka client config 3. added route definitions,predicates and filters

---
+++ src/main/java/com/github/mitrakumarsujan/gatewayservice/configuration/DiscoveryClientConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.mitrakumarsujan.gatewayservice.configuration;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaClient
+public class DiscoveryClientConfiguration {
+}
